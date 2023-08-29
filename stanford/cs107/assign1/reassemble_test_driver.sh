98dbfb07a0efd1be9c09ac5c558189a8977db7a7
Liz H <lizmeow@gmail.com>
2023-08-28 23:45:16 -0400

    add test harness

---
+++ stanford/cs107/assign1/reassemble_test_driver.sh
@@ -0,0 +1,21 @@
+#/bin/bash -x
+
+TEST_DIR=test_set
+N_TESTS=15
+ERROR_FLAG=0
+
+for i in $(seq 1 $N_TESTS);
+do
+    ./reassemble $TEST_DIR/test$i.txt > $TEST_DIR/test$i.out 2>&1
+    diff $TEST_DIR/test$i.out $TEST_DIR/test$i.ref
+    if [ $? -ne 0 ]; then
+        printf "$TEST_DIR/test$i.txt input file did not pass.\n"
+        ERROR_FLAG=1
+    fi
+done
+
+if [ $ERROR_FLAG -ne 0 ]; then
+    printf "Not all tests passed.\n"
+else
+    printf "All tests passed.\n"
+fi
