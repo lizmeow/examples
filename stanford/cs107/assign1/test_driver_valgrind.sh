ee0f55d1c37f3770637666ed4df8e3efedf62013
Liz H <lizmeow@gmail.com>
2023-09-06 10:08:47 -0400

    added -q option to grep

--- stanford/cs107/assign1/test_driver_valgrind.sh
+++ stanford/cs107/assign1/test_driver_valgrind.sh
@@ -11,7 +11,7 @@ make -f Makefile.debug
 for i in $(seq 1 $N_TESTS);
 do
     r=$(valgrind ./reassemble $TEST_DIR/test$i.txt 2>&1)
-    echo "$r" | grep "$KEY_PHRASE" > /dev/null 
+    echo "$r" | grep -q "$KEY_PHRASE" 
     if [ $? -ne 0 ]; then
         printf "$TEST_DIR/test$i.txt did not pass valgrind.\n"
         ERROR_FLAG=1
